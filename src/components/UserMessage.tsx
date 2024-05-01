b67d8b07689cf534dff5997090da0e301a133106
JooHee Gwon <joohee06197@gmail.com>
2024-05-01 16:36:26 +0900

    누군가의 디자인 피드백 반영

--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -5,6 +5,7 @@ import {
 } from '@chakra-ui/react';
 import { Message } from '../schemas/Message';
 import { Avatar } from '@chakra-ui/react'
+import { getAuthData } from '../utils/auth';
 type MessageProps = {
     index: number;
     message: Message;
@@ -13,12 +14,15 @@ type MessageProps = {
 
 export const UserMessage: React.FC<MessageProps> = ({ index, message, flexDirection }) => {
     console.log(index, flexDirection)
+    const authData = getAuthData();
+
     return (<HStack key={index} w="100%" flexDirection={flexDirection} gap={4}>
-        <Box>
-            <Avatar src='https://bit.ly/broken-link' />
+        <Avatar src='https://bit.ly/broken-link' />
+        <Box display="flex" flexDirection="column" gap={2}>
+            <Text textAlign="right" fontSize="md" color="gray.900" fontWeight={700} mr={2}>{authData?.user.username}</Text>
+            < Box p={3} bg="#FFD700" borderRadius="lg" >
+                <Text color="black">{message.content}</Text>
+            </Box >
         </Box>
-        < Box p={3} bg="#FFD700" borderRadius="lg" >
-            <Text color="black">{message.content}</Text>
-        </Box >
     </HStack >)
 }
